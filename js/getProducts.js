3de84604af3a39fd790c6f10f30b93f7d5db8388
ayjammoulMTS <jammoul.171@gmail.com>
2023-06-14 16:00:49 +0300

    Get products + server cnxn

--- js/getProducts.js
+++ js/getProducts.js
@@ -1,40 +1,63 @@
 // Fetch the products from the server
-fetch('http://localhost:8000/products')
+fetch('https://6486fd6fb28f780008e3f1e2--aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/products')
     .then(response => response.json())
     .then(products => {
-        const productContainer = document.querySelector('#property-carousel');
+        const productContainer = document.querySelector('#property-grid-item');
         // Loop through each product
         products.forEach(product => {
           console.log(product)
             // Create the product elements
             const productElem = document.createElement('div');
             productElem.classList.add('carousel-item-b');
+            productElem.classList.add('col-md-4');
+
 
             productElem.innerHTML = `
-            <div class="card-box-a card-shadow">
-              <div class="img-box-a">
-                <img src="img/project1.2.jpeg" alt="" class="img-a img-fluid">
-              </div>
-              <div class="card-overlay">
-                <div class="card-overlay-a-content">
-                  <div class="card-header-a">
-                    <h2 class="card-title-a">
-                      <a href="property-single.html">${product.name}</a>
-                    </h2>
+          <div class="card-box-a card-shadow">
+            <div class="img-box-a">
+              <img src="img/property-1.jpg" alt="" class="img-a img-fluid">
+            </div>
+            <div class="card-overlay">
+              <div class="card-overlay-a-content">
+                <div class="card-header-a">
+                  <h2 class="card-title-a">
+                    <a href="#">${product.name}
+                      <br /> ${product.name}</a>
+                  </h2>
+                </div>
+                <div class="card-body-a">
+                  <div class="price-box d-flex">
+                    <span class="price-a">rent | ${product.age}</span>
                   </div>
-                  <div class="card-body-a">
-                    <div class="price-box d-flex">
-                      <span class="price-a">${product.age}</span>
-                    </div>
-                    <a href="property-single.html" class="link-a">Click here to view
-                      <span class="ion-ios-arrow-forward"></span>
-                    </a>
-                  </div>
-                  <div class="card-footer-a">
-                  </div>
+                  <a href="property-single.html" class="link-a">Click here to view
+                    <span class="ion-ios-arrow-forward"></span>
+                  </a>
+                </div>
+                <div class="card-footer-a">
+                  <ul class="card-info d-flex justify-content-around">
+                    <li>
+                      <h4 class="card-info-title">Area</h4>
+                      <span>340m
+                        <sup>2</sup>
+                      </span>
+                    </li>
+                    <li>
+                      <h4 class="card-info-title">Beds</h4>
+                      <span>2</span>
+                    </li>
+                    <li>
+                      <h4 class="card-info-title">Baths</h4>
+                      <span>4</span>
+                    </li>
+                    <li>
+                      <h4 class="card-info-title">Garages</h4>
+                      <span>1</span>
+                    </li>
+                  </ul>
                 </div>
               </div>
-            </div>`;
+            </div>
+          </div>`;
 
             // Add the product element to the product container
             productContainer.appendChild(productElem);
